974054d411ddf28ae8fda23acc8b207a8317fc55
Richard Groves <richard@noodlfroot.com>
2015-10-16 16:48:08 +0100

    Removing orientation/rotation code from TVOS platform

--- cocos2d/Platforms/iOS/CCDirectorIOS.m
+++ cocos2d/Platforms/iOS/CCDirectorIOS.m
@@ -191,6 +191,7 @@
 		}
 }
 
+#if !defined(__CC_PLATFORM_TVOS)
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
@@ -200,6 +201,7 @@
 
 	return ret;
 }
+#endif
 
 // Commented. See issue #1453 for further info: http://code.google.com/p/cocos2d-iphone/issues/detail?id=1453
 //-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
